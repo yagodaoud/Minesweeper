688c233d6e00bf9e3e34355102702c056346dbba
Yago Daoud <yagodaoud@hotmail.com>
2023-04-18 09:16:56 -0300

    Starting to build the UI with Swing

--- src/br/com/yagodaoud/ms/exceptions/ExplosionException.java
+++
@@ -1,8 +0,0 @@
-package br.com.yagodaoud.ms.exceptions;
-
-import java.io.Serial;
-
-public class ExplosionException extends RuntimeException{
-    @Serial
-    private static final long serialVersionUID = 1L;
-}
